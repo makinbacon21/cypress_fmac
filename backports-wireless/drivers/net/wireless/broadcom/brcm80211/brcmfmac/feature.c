227a529e81efe05a3625b3eeabd83307db6eb560
nannaiah <nannaiah@nvidia.com>
2019-01-22 13:54:54 -0800

    brcfmac: Disable Multi Channel Concurrency
    Issue: AGO should follow station channel so if
    MCC is enabled then wpa supplicant doesn't check for
    freq conflict and ends up AGO and station on different
    channel.
    
    Fix: Disable Multi Channel Concurrency.
    
    NvTag: ShieldCommsAccessories
    NvFeature: Blake Support
    
    Bug 200465854
    Bug 1410893
    
    Signed-off-by: Nagaraj Annaiah <nannaiah@nvidia.com>
    Change-Id: I4519e56d7162af78c5cd1f4bb91478c14604ca68
    Reviewed-on: https://git-master.nvidia.com/r/2001255
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    (cherry picked from commit 0017efb36b4c265e75e4afc433c4dd83caf59cb5)
    Reviewed-on: https://git-master.nvidia.com/r/2004214
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- backports-wireless/drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
+++ backports-wireless/drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
@@ -34,7 +34,7 @@ struct brcmf_feat_fwcap {
 
 static const struct brcmf_feat_fwcap brcmf_fwcap_map[] = {
 	{ BRCMF_FEAT_MBSS, "mbss" },
-	{ BRCMF_FEAT_MCHAN, "mchan" },
+//	{ BRCMF_FEAT_MCHAN, "mchan" },
 	{ BRCMF_FEAT_P2P, "p2p" },
 	{ BRCMF_FEAT_MONITOR, "monitor" },
 	{ BRCMF_FEAT_MONITOR_FMT_RADIOTAP, "rtap" },
