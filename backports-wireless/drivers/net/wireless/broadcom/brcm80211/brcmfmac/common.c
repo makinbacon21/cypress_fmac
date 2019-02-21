5fc0445505d24e724621cd9ed28527dc12151ef8
Mohan Thadikamalla <mohant@nvidia.com>
2019-02-21 17:05:00 +0530

    Add and enable Nv Roam Off support
    Issue:
    Internal Roaming should be off for tegra
    platforms
    
    Fix:
    Add an enable default roam off support
    for Tegra platform
    
    NvTag: NvWifi
    NvFeature: NvRoamOff
    Bug 200483720
    
    Change-Id: I51495c7be6cab27da82ef05a15ee760ee5975680
    Signed-off-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2024889
    Reviewed-by: Om Prakash Singh <omp@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Shobek Attupurath <sattupurath@nvidia.com>
    Reviewed-by: Nagaraj Annaiah <nannaiah@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- backports-wireless/drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ backports-wireless/drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -92,7 +92,11 @@ static int brcmf_fcmode;
 module_param_named(fcmode, brcmf_fcmode, int, 0);
 MODULE_PARM_DESC(fcmode, "Mode of firmware signalled flow control");
 
+#ifdef CPTCFG_NV_CUSTOM_ROAM_OFF
+static int brcmf_roamoff = 1;
+#else
 static int brcmf_roamoff;
+#endif
 module_param_named(roamoff, brcmf_roamoff, int, 0400);
 MODULE_PARM_DESC(roamoff, "Do not use internal roaming engine");
 
